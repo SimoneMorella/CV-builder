52c3f0b7cdb72cbc4491b8377ca0400fc4fd263d
SimoneMorella <simonemorella21@gmail.com>
2024-08-01 19:50:40 +0200

    fixed added profile pic btn

--- CV-Builder/src/components/personalInfo.jsx
+++ CV-Builder/src/components/personalInfo.jsx
@@ -38,11 +38,11 @@ export default function PersonalInfo({data, onInput, addImg, profilePic}) {
                         data-id={jobName.id}
                         onChange={onInput}
                         maxLength={40}
-                        className="w-[70%] p-3 max-[533px]:py-2 max-[533px]:text-sm rounded-xl outline-none bg-darkCop transition duration-300 ease-in-out focus:bg-inputFocus hover:bg-inputFocus" />
+                        className="w-full p-3 max-[533px]:py-2 max-[533px]:text-sm rounded-xl outline-none bg-darkCop transition duration-300 ease-in-out focus:bg-inputFocus hover:bg-inputFocus" />
                     <div>
                         <label 
                             htmlFor="file"
-                            className=" max-[533px]:p-1 p-3 max-[533px]:text-xs flex gap-1 items-center bg-white text-darkCop font-semibold rounded-xl cursor-pointer transition shadow-unpressed active:shadow-pressed active:scale-95 hover:shadow-pressed"
+                            className=" p-3 max-[533px]:py-2 w-44 max-[533px]:w-[137px] max-[533px]:text-xs flex gap-1 items-center bg-white text-darkCop font-semibold rounded-xl cursor-pointer transition shadow-unpressed active:shadow-pressed active:scale-95 hover:shadow-pressed"
                         >
                             <i className='bx bxs-image-add text-2xl max-[533px]:text-xl'></i>
                             <span>Add profile pic</span>
